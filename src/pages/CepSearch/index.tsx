d816ad9e8918bef05c808d85f1093664578cee40
Luiz Mendes <emailluizMendes@gmail.com>
2023-03-13 20:22:45 -0300

    first commit

---
+++ src/pages/CepSearch/index.tsx
@@ -0,0 +1,76 @@
+import './styles.css';
+
+import ResultCard from 'components/ResultCard';
+import { useState } from 'react';
+import axios from 'axios';
+
+type FormData = {
+  cep: string;
+};
+
+type Address = {
+  logradouro: string;
+  localidade: string;
+};
+
+const CepSearch = () => {
+  const [address, setAddress] = useState<Address>();
+
+  const [formData, setFormData] = useState<FormData>({
+    cep: '',
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    /* console.log("mudou") */
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setFormData({ ...formData, [name]: value }); // aproveita todos os dados do formData e adiciona no campo com o name o valor passado
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault(); // impede o envio da tela e a limpeza dos dados
+    axios
+      .get(`https://viacep.com.br/ws/${formData.cep}/json/`)
+      .then((response) => {
+        setAddress(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        setAddress(undefined);
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className="cep-search-container">
+      <h1 className="text-primary">Busca CEP</h1>
+      <div className="container search-container">
+        <form onSubmit={handleSubmit}>
+          <div className="form-container">
+            <input
+              type="text"
+              className="search-input"
+              placeholder="CEP (somente números)"
+              onChange={handleChange}
+              name="cep"
+              value={formData.cep}
+            />
+            <button type="submit" className="btn btn-primary search-button">
+              Buscar
+            </button>
+          </div>
+        </form>
+
+        {address && (
+          <>
+            <ResultCard title="Logradouro" description={address.logradouro} />
+            <ResultCard title="Localidade" description={address.localidade} />
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default CepSearch;
