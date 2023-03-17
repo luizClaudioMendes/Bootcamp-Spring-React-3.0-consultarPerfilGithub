ae9ddb9d80683f8b0e916b08d56c686c1a9f890a
Luiz Mendes <emailluizMendes@gmail.com>
2023-03-17 10:08:38 -0300

    final exercicio

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,13 +3,15 @@ import { Link } from 'react-router-dom';
 import './styles.css';
 
 const Home = () => {
-    return (
-        <div className="home-container">
-            <Link to="/cepsearch">
-                <button className="btn btn-primary btn-lg start-button">Iniciar</button>
-            </Link>
-        </div>
-    );
-}
+  return (
+    <div className="home-container">
+        <h1>Desafio Github API</h1>
+        <h3>DevSuperior - Escola de programação</h3>
+      <Link to="/githubsearch">
+        <button className="btn btn-primary btn-lg start-button">Começar</button>
+      </Link>
+    </div>
+  );
+};
 
 export default Home;
